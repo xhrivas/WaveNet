be266fbf141834c2bbda3171924b45f4b9dae776
Bhumika Shrivas <bhumikashrivas.work@gmail.com>
2025-08-17 02:31:24 +0530

    Update gesture_recognition.py

--- gesture_recognition.py
+++ gesture_recognition.py
@@ -2,28 +2,26 @@ import cv2
 import mediapipe as mp
 
 # Initialize MediaPipe Hands
-mpHands = mp.solutions.hands
-hands = mpHands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.7)
-mpDraw = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.7)
+mp_draw = mp.solutions.drawing_utils
 
 # Finger tip landmark indices
-tipIds = [4, 8, 12, 16, 20]
+tip_ids = [4, 8, 12, 16, 20]
 
-# Start video capture
-cap = cv2.VideoCapture(0)
-
-def get_finger_states(lmList):
+def get_finger_states(lm_list):
+    """Returns list of finger states: 1 for open, 0 for closed."""
     fingers = []
 
     # Thumb
-    if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:  # Right hand logic
+    if lm_list[tip_ids[0]][1] > lm_list[tip_ids[0] - 1][1]:
         fingers.append(1)
     else:
         fingers.append(0)
 
-    # Fingers
+    # Other four fingers
     for id in range(1, 5):
-        if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+        if lm_list[tip_ids[id]][2] < lm_list[tip_ids[id] - 2][2]:
             fingers.append(1)
         else:
             fingers.append(0)
@@ -31,48 +29,44 @@ def get_finger_states(lmList):
     return fingers
 
 def classify_gesture(fingers):
-    # Simple hardcoded examples
+    """Maps finger states to simple gestures/letters."""
     if fingers == [0, 0, 0, 0, 0]:
-        return "A"
+        return "A"   # Fist
     elif fingers == [0, 1, 1, 1, 1]:
-        return "B"
+        return "B"   # Open hand
     elif fingers == [0, 1, 0, 0, 0]:
-        return "D"
-    elif fingers == [0, 1, 1, 0, 0]:
-        return "L"
-    elif fingers == [0, 1, 1, 1, 0]:
-        return "W"
-    elif fingers == [1, 1, 1, 1, 1]:
-        return "5"
+        return "D"   # Index finger up
     else:
-        return "-"
+        return "-"   # Unknown
+
+# Start video capture
+cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
-    img = cv2.flip(img, 1)  # Mirror image
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
+    img = cv2.flip(img, 1)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_rgb)
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            lmList = []
+            lm_list = []
             h, w, _ = img.shape
             for id, lm in enumerate(handLms.landmark):
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                lmList.append([id, cx, cy])
+                lm_list.append([id, cx, cy])
 
-            if lmList:
-                fingers = get_finger_states(lmList)
+            if lm_list:
+                fingers = get_finger_states(lm_list)
                 letter = classify_gesture(fingers)
-                cv2.putText(img, f'Letter: {letter}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX,
-                            2, (0, 255, 0), 3)
+                cv2.putText(img, f'Letter: {letter}', (10, 70),
+                            cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
 
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
-            
+            mp_draw.draw_landmarks(img, handLms, mp_hands.HAND_CONNECTIONS)
 
-    cv2.imshow("Gesture to Alphabet", img)
+    cv2.imshow("Gesture Recognition Prototype", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
+
 cap.release()
 cv2.destroyAllWindows()
